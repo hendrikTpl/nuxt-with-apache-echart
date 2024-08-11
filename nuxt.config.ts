db600fb387ecbcf1afa663ca14617145da30662b
hendrikTpl <hendrik.gian@gmail.com>
2024-08-11 14:40:05 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: 'Nuxt3 with Apache Echart starter',
+      htmlAttrs: {
+        lang: 'en'
+      },
+
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+    },
+  },
+
+  modules: [],
+  ssr: true,
+  plugins: [],
+
+  build: {
+    transpile: ['vuetify', 'echarts', 'zrender', 'tslib'],
+  },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 3010,
+  },
+
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL
+    },
+  },
+
+  compatibilityDate: '2024-08-11'
 })
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <client-only>
+      <VChart class="chart" :option="option" />
+    </client-only>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PieChart } from 'echarts/charts'
+import {
+  GridComponent,
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+} from 'echarts/components'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { ref } from 'vue'
+import VChart from 'vue-echarts'
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+])
+const option = ref({
+  title: {
+    text: 'Traffic Sources',
+    left: 'center',
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: '{a} <br/>{b} : {c} ({d}%)',
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left',
+    data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+  },
+  series: [
+    {
+      name: 'Traffic Sources',
+      type: 'pie',
+      radius: '55%',
+      center: ['50%', '60%'],
+      data: [
+        { value: 335, name: 'Direct' },
+        { value: 310, name: 'Email' },
+        { value: 234, name: 'Ad Networks' },
+        { value: 135, name: 'Video Ads' },
+        { value: 1548, name: 'Search Engines' },
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+  ],
+});
+
+</script>
+<style scoped>
+.chart {
+  height: 100vh;
+}
+</style>
